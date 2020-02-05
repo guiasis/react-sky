d7c3307527a6307896f3f21f1c9201fddaebe3a5
Guilherme Assis <guilherme.assis@NOT-MED-056.local>
2020-02-05 16:10:33 -0300

    Rodando já

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.png';
+import logo from 'logo.png';
 
 
 function App() {
   return (
     <div style = {style.back}>
+        <img src={logo} alt="logo"></img>
         <input type = "Text" style = {style.input} placeholder = "Login"></input>
         <input type = "Text" style = {style.input} placeholder = "Senha"></input>
+        <button style = {{fontSize:'30px', borderRadius:'5px',padding:'5px'}}>LOGAR</button>
       
     </div>
   );
@@ -14,12 +16,24 @@ function App() {
 
 
 let style = {input:{
+
 display:'flex',
+alignItems: 'center',
 textAlign:'center',
 margin: '10px',
-padding: '5px'
+padding: '5px',
+borderRadius:'5px',
+fontSize:'15px'
+
 },
 back:{
-
+  display:'flex',
+  flexDirection:'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#C0C0C0',
+  boxSizing: "border box",
+  width: '100vw', 
+  height: '100vh'
 },};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
