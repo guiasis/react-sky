58c4645d6b759cd35db4a118a1e90d6625c9c522
Guilherme Assis <guilherme.assis@NOT-MED-056.local>
2020-02-05 15:19:54 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {style.back}>
+        <input type = "Text" style = {style.input} placeholder = "Login"></input>
+        <input type = "Text" style = {style.input} placeholder = "Senha"></input>
+      
     </div>
   );
 }
 
+
+let style = {input:{
+display:'flex',
+textAlign:'center',
+margin: '10px',
+padding: '5px'
+},
+back:{
+
+},};
 export default App;
